6bfa7705686dc7eab1c458ede6d1b704ff9e65c5
Ali Foroozan <Foroozan@veerasense.local>
2022-05-16 16:34:57 +0430

    access token Fixed

--- app/src/main/java/com/frn/meditradenttokenauth/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/frn/meditradenttokenauth/data/remote/RemoteDataSource.kt
@@ -1,9 +1,12 @@
 package com.frn.meditradenttokenauth.data.remote
 
 import android.content.Context
-import android.util.Log
 import com.frn.meditradenttokenauth.BuildConfig
+import com.frn.meditradenttokenauth.data.repository.DataStoreOperationsImpl
+import com.frn.meditradenttokenauth.domain.repository.DataStoreOperations
 import com.frn.meditradenttokenauth.utils.Constants.BASE_URL
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,33 +15,45 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RemoteDataSource {
 
+
     fun <Api> buildApi(
         api: Class<Api>,
         context: Context
     ): Api {
-        val authenticator = TokenAuthenticator(context, buildTokenApi())
+        val authenticator =
+            TokenAuthenticator(buildTokenApi(context), DataStoreOperationsImpl(context))
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(getRetrofitClient(authenticator))
+            .client(getRetrofitClient(authenticator, DataStoreOperationsImpl(context = context)))
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
             .create(api)
     }
 
-    private fun buildTokenApi(): MeditradentApi {
+    private fun buildTokenApi(context: Context): MeditradentApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(getRetrofitClient())
+            .client(getRetrofitClient(dataStoreOperations = DataStoreOperationsImpl(context = context)))
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
             .create(MeditradentApi::class.java)
     }
 
-    private fun getRetrofitClient(authenticator: Authenticator? = null): OkHttpClient {
+    private fun getRetrofitClient(
+        authenticator: Authenticator? = null,
+        dataStoreOperations: DataStoreOperations
+    ): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor { chain ->
                 chain.proceed(chain.request().newBuilder().also {
-                    it.addHeader("Accept", "application/json")
+                    runBlocking {
+                        val accessToken = dataStoreOperations.readToken().first()
+                        if (accessToken.isNotEmpty()) {
+                            it.header("Authorization", "Bearer $accessToken")
+                        }
+                        it.addHeader("Accept", "application/json")
+                    }
                 }.build())
             }.also { client ->
                 authenticator?.let { client.authenticator(it) }
--- app/src/main/java/com/frn/meditradenttokenauth/data/remote/TokenAuthenticator.kt
+++ app/src/main/java/com/frn/meditradenttokenauth/data/remote/TokenAuthenticator.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.frn.meditradent.domain.model.Registration
 import com.frn.meditradenttokenauth.core.SafeApiCall
 import com.frn.meditradenttokenauth.data.repository.DataStoreOperationsImpl
+import com.frn.meditradenttokenauth.domain.repository.DataStoreOperations
 import com.frn.meditradenttokenauth.utils.Resource
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -14,18 +15,15 @@ import okhttp3.Route
 import javax.inject.Inject
 
 class TokenAuthenticator @Inject constructor(
-    context: Context,
-    private val api: MeditradentApi
+    private val api: MeditradentApi,
+    private val dataStoreOperations: DataStoreOperations
 ) : Authenticator , SafeApiCall {
-
-    private val appContext = context.applicationContext
-    private val userPreferences = DataStoreOperationsImpl(appContext)
 
     override fun authenticate(route: Route?, response: Response): Request? {
         return runBlocking {
             when (val tokenResponse = getUpdatedToken()) {
                 is Resource.Success -> {
-                    userPreferences.saveToken(
+                    dataStoreOperations.saveToken(
                         tokenResponse.value.access_token,
                         tokenResponse.value.refresh_token
                     )
@@ -39,8 +37,9 @@ class TokenAuthenticator @Inject constructor(
     }
 
     private suspend fun getUpdatedToken(): Resource<Registration> {
-        val refreshToken = userPreferences.readRefreshToken().first()
+        val refreshToken = dataStoreOperations.readRefreshToken().first()
         return safeApiCall { api.refreshAccessToken(refreshToken) }
     }
 
 }
+
